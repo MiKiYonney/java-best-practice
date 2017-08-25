528f5b58ffb3fbeea56dedb9001388435e97f0a5
yonney.yang <yonney.yang@dianping.com>
2017-08-25 11:11:56 +0800

    Add

--- stream-validator/src/main/java/com/yonney/stram/validator/dto/AddTechnicianRespDTO.java
+++ stream-validator/src/main/java/com/yonney/stram/validator/dto/AddTechnicianRespDTO.java
@@ -6,4 +6,24 @@ import java.io.Serializable;
  * Created by yonney on 17/8/24.
  */
 public class AddTechnicianRespDTO implements Serializable{
+    private int respCode;
+    private String respMsg;
+
+    public int getRespCode() {
+        return respCode;
+    }
+
+    public AddTechnicianRespDTO setRespCode(int respCode) {
+        this.respCode = respCode;
+        return this;
+    }
+
+    public String getRespMsg() {
+        return respMsg;
+    }
+
+    public AddTechnicianRespDTO setRespMsg(String respMsg) {
+        this.respMsg = respMsg;
+        return this;
+    }
 }
---
+++ stream-validator/src/main/java/com/yonney/stram/validator/framework/MethodValidateInterceptor.java
@@ -0,0 +1,39 @@
+package com.yonney.stram.validator.framework;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.stereotype.Component;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.util.Set;
+import static com.google.common.collect.Iterables.getFirst;
+
+/**
+ * Created by yonney on 17/8/25.
+ */
+@Component
+public class MethodValidateInterceptor implements MethodInterceptor {
+    private final Validator validator;
+
+    public MethodValidateInterceptor() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+    public Object invoke(MethodInvocation methodInvocation) throws Throwable {
+        //获取参数，并检查是否应该验证
+        Object[] arguments = methodInvocation.getArguments();
+        for (Object argument : arguments) {
+
+            Set<ConstraintViolation<Object>> constraintViolations = validator.validate(argument);
+            ConstraintViolation<Object> constraintViolation = getFirst(constraintViolations, null);
+            if (constraintViolation == null) {
+                continue;
+            }
+
+            throw new RuntimeException(constraintViolation.getPropertyPath() + " " + constraintViolation.getMessage());
+        }
+        return methodInvocation.proceed();
+    }
+}
